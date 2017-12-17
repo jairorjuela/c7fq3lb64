8985adf248ba4698b0b01d765ff357f7d565f8e4
jairorjuela <genius85862@gmail.com>
2017-12-17 12:01:31 -0500

    Devise

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,8 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
      @expenses = Expense.order("date DESC")
+     @expenses = @expenses.where("concept LIKE :concept", concept: "%#{params[:concept]}%")
+     @expenses = @expenses.where("category_id LIKE :category_id", category_id:     "%#{params[:category_id]}%")
   end
 end
