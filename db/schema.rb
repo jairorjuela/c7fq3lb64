338a9c5b6b96cd775bdb843f9084a1df51ec2493
jairorjuela <genius85862@gmail.com>
2017-12-17 14:07:39 -0500

    Devise_1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217164746) do
+ActiveRecord::Schema.define(version: 20171217190121) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171217164746) do
     t.decimal  "amount"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
